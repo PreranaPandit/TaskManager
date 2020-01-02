12cf54f797ffbdcffcad7d1b3f8435c6cbb5ac54
PreranaPandit <preranapandit07@gmail.com>
2020-01-02 09:02:26 +0545

    done till strictmode

---
+++ app/src/main/java/com/example/prerana/taskmanager/serverresponse/ImageResponse.java
@@ -0,0 +1,19 @@
+package com.example.prerana.taskmanager.serverresponse;
+
+public class ImageResponse {
+
+    private String fileName;
+
+    public ImageResponse(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
+
