5b88be6db9393fc72495b872f9ee6cf210f87802
PreranaPandit <preranapandit07@gmail.com>
2020-01-05 12:45:43 +0545

    Gallery images opens in sign up form

--- app/src/main/java/com/example/prerana/taskmanager/activities/SignUpActivity.java
+++ app/src/main/java/com/example/prerana/taskmanager/activities/SignUpActivity.java
@@ -1,14 +1,25 @@
 package com.example.prerana.taskmanager.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.prerana.taskmanager.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    ImageView imgProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +32,59 @@ public class SignUpActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_sign_up);
+
+
+        //data binding
+        imgProfile = findViewById(R.id.imgProfile);
+
+        imgProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BrowseImage();
+            }
+        });
+    }
+
+    //browse image function
+    private void BrowseImage()
+    {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+
+        //browse only image
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK)
+        {
+            if(data == null)
+            {
+                Toast.makeText(this, "Please select an image!!.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        Uri uri = data.getData();
+
+        imgProfile.setImageURI(uri);
+        String imgPath = getRealPathFromUri(uri);
+        Toast.makeText(this, "emulator/images/"+imgPath, Toast.LENGTH_SHORT).show();
+    }
+
+    private String getRealPathFromUri(Uri uri)
+    {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(), uri, projection, null,null, null);
+        Cursor cursor = loader.loadInBackground();
+
+        int colIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return result;
     }
 }
